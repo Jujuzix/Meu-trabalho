ab7f3d000e3e9e7dd4adb880ef1929a4e5ce1718
Jujuzix <maronez.juliana@gmail.com>
2023-10-19 14:11:55 -0400

    PArte1

--- app.js
+++ app.js
@@ -9,9 +9,48 @@ app.get('/', (req, res) => {
   res.render('index', { message: 'Produtos Principais' });
 });
 
-app.get('/produtodes', (req, res) => {
-  res.render('produtodes', { message: ' Destaq' });
+app.get('/produtodesq', (req, res) => {
+  res.render('produtodesq', { message: ' Mais' });
 });
+
+app.get('/prod', (req, res) => {
+  res.render('prod', { message: ' Sobre' });
+});
+
+app.get('/prod1', (req, res) => {
+  res.render('prod1', { message: ' Sobre1' });
+});
+
+app.get('/prod2', (req, res) => {
+  res.render('prod2', { message: ' Sobre2' });
+});
+
+app.get('/prod3', (req, res) => {
+  res.render('prod3', { message: ' Sobre3' });
+});
+
+app.get('/prod4', (req, res) => {
+  res.render('prod4', { message: ' Sobre4' });
+});
+
+const produtos = [
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+
+]
+
+app.get('/prod5', (req, res) => {
+  res.render('prod5', { message: ' Sobre5' });
+});
+
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
