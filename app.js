2394da2d6b791e3efb2327f5113768a5e374a1b2
josecardozoif <maronez.juliana@gmail.com>
2023-10-26 14:22:59 -0400

    Bem feito até

--- app.js
+++ app.js
@@ -34,9 +34,9 @@ app.get('/prod4', (req, res) => {
 });
 
 const produtos = [
+  {id: 1, nome:"Placa de Vídeo RX ", preco:"2,149,99", descrica:"", img:" https://images.kabum.com.br/produtos/fotos/475438/placa-de-video-rx-6750-xt-mech-2x-12g-v1-radeon-12gb-gddr6-freesync-dual-fan_1689965385_g.jpg"},
   {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
-  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
-  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
+  {id: 1, nome:"nome", preco:"400", descrica:"Descrição"}, 
   {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
   {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
   {id: 1, nome:"nome", preco:"400", descrica:"Descrição"},
@@ -51,6 +51,25 @@ app.get('/prod5', (req, res) => {
   res.render('prod5', { message: ' Sobre5' });
 });
 
+app.get('/prod6', (req, res) => {
+  res.render('prod6', { message: ' Sobre6' });
+});
+
+app.get('/prod7', (req, res) => {
+  res.render('prod7', { message: ' Sobre7' });
+});
+
+app.get('/prod8', (req, res) => {
+  res.render('prod8', { message: ' Sobre8' });
+});
+
+app.get('/prod9', (req, res) => {
+  res.render('prod9', { message: ' Sobre9' });
+});
+
+app.get('/prod10', (req, res) => {
+  res.render('prod10', { message: ' Sobre10' });
+});
 
 app.listen(port, () => {
   console.log(`Servidor rodando em http://localhost:${port}`);
